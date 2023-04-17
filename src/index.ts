0c848405b852b3eee6739092f3ed9694e67ecdce
tenthirtyone <alexsherbuck@gmail.com>
2023-04-16 19:44:30 -0500

    env file, constructor, nodemon shortcut

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("boilerplate");
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export default class UniMonitor {
+  constructor() {
+    const { INFURA_KEY } = process.env;
+    if (!INFURA_KEY) {
+      console.log("Please set the INFURA_KEY env var in .env");
+      return;
+    }
+  }
+  start() {
+    console.log("UniMonitor Started");
+  }
+}
+
+// I do this for nodemon / hot reloading in dev
+// $ nodemon src/index.ts
+if (require.main === module) {
+  const monitor = new UniMonitor();
+  monitor.start();
+}
