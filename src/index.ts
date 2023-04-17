741809be864c4a7ef04ba64899e08573077b93ae
tenthirtyone <alexsherbuck@gmail.com>
2023-04-16 19:56:33 -0500

    constants, block listener from ethers

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+import { ethers } from "ethers";
+import { MAINNET, DAI_ADDRESS, USDC_ADDRESS, WETH_ADDRESS } from "./constants";
 
 export default class UniMonitor {
+  private provider;
   constructor() {
     const { INFURA_KEY } = process.env;
     if (!INFURA_KEY) {
       console.log("Please set the INFURA_KEY env var in .env");
-      return;
     }
+
+    this.provider = new ethers.providers.InfuraProvider(MAINNET, INFURA_KEY);
+
+    process.on("SIGINT", () => {
+      console.log("Shutting down, removing listeners");
+      this.provider.removeListener("block", this.onNewBlock);
+    });
   }
+
   start() {
     console.log("UniMonitor Started");
+    this.provider.on("block", this.onNewBlock);
+  }
+
+  onNewBlock(block: string) {
+    console.log(block);
   }
 }
 
